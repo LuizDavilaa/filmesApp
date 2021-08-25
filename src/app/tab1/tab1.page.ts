f26cc12916488bcad031f317c338d4745c3c7fbf
LuizDavilaa <luiz.davila@etec.sp.gov.br>
2021-08-25 10:13:17 -0300

    Declaração, utilização de variáveis e criação de interfaces

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,7 +1,7 @@
+import { IFilme } from './../models/IFilme.module';
 import { Component } from '@angular/core';
 import { AlertController } from '@ionic/angular';
 import { ToastController } from '@ionic/angular';
-
 
 @Component({
   selector: 'app-tab1',
@@ -9,9 +9,33 @@ import { ToastController } from '@ionic/angular';
   styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
-  titulo = 'Filmes';
+  title = 'Filmes';
 
-  constructor(public alertController: AlertController, public toastController: ToastController) {}
+  listaVideos: IFilme[] = [
+    {
+      nome: 'Parasita (2019)',
+      lancamento: '07/11/2019',
+      duracao: '2h 12m',
+      classificacao: 85,
+      cartaz:
+        'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/igw938inb6Fy0YVcwIyxQ7Lu5FO.jpg',
+      generos: ['Comédia', 'Thriller', 'Drama'],
+    },
+    {
+      nome: 'O Auto da Compadecida (2000)',
+      lancamento: '15/09/2000',
+      duracao: '1h 44m',
+      classificacao: 85,
+      cartaz:
+        'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/m8eFedsS7vQCZCS8WGp5n1bVD0q.jpg',
+      generos: ['Aventura', 'Comédia'],
+    },
+  ];
+
+  constructor(
+    public alertController: AlertController,
+    public toastController: ToastController
+  ) {}
 
   async exibirAlertaFavorito() {
     const alert = await this.alertController.create({
@@ -23,14 +47,15 @@ export class Tab1Page {
           role: 'cancel',
           handler: (blah) => {
             console.log('Confirm Cancel: blah');
-          }
-        }, {
+          },
+        },
+        {
           text: 'SIM, favoritar!',
           handler: () => {
             this.apresentarToast();
-          }
-        }
-      ]
+          },
+        },
+      ],
     });
 
     await alert.present();
@@ -40,10 +65,8 @@ export class Tab1Page {
     const toast = await this.toastController.create({
       message: 'Filme adicionado aos favoritos.',
       duration: 2000,
-      color: 'green'
+      color: 'green',
     });
     toast.present();
   }
-
-
-  }
+}
