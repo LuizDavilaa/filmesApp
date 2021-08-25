eb1d3ed7b93e33e2838183d85a42ad426c5b5222
LuizDavilaa <luiz.davila@etec.sp.gov.br>
2021-08-25 09:24:31 -0300

    Adicionando filmes e sliding

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { ToastController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
+  titulo = 'Filmes';
 
-  constructor() {}
+  constructor(
+    public route: Router
+  ) {}
 
-}
+  }
