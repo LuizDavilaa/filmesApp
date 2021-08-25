30a25475e9bdcca02629a4902c9c3def79f25f29
LuizDavilaa <luiz.davila@etec.sp.gov.br>
2021-08-25 12:06:40 -0300

    Criação de serviço e model para API de filmes

---
+++ src/app/services/filme.service.ts
@@ -0,0 +1,41 @@
+import { HttpClient } from '@angular/common/http';
+import { IListaFilmes } from './../models/IFilmeAPI.model';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+import { ToastController } from '@ionic/angular';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FilmeService {
+
+  lingua = 'pt-BR';
+  regiao = 'BR';
+
+  private apiURL = 'https://api.themoviedb.org/3';
+  private key = '?api_key=9d59c60138c043b6aeae2709f6b85d4c';
+
+  constructor(private http: HttpClient, public toastController: ToastController) { }
+
+  buscarFilmes(busca: string): Observable<IListaFilmes>{
+    const url = `$(this.apiURL)search/movie${this.key}&language=${this.lingua}&region=${this.regiao}&query=${busca}`;
+
+    return this.http.get<IListaFilmes>(url).pipe(
+      map(retorno => retorno),
+      catchError(erro => this.exibirErro(erro))
+    );
+  }
+
+  async exibirErro(erro) {
+    const toast = await this.toastController.create({
+      message: 'Erro ao consultar a API!',
+      duration: 2000,
+      color: 'danger',
+      position: 'middle'
+    });
+    toast.present();
+    return null;
+  }
+}
